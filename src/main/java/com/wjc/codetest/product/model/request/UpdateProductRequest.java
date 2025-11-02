1d4b433a45054c1756089a147ffab32c530ba526
조영무 <fprh13@gmail.com>
2025-11-02 21:15:06 +0900

    review: 요청 Dto의 기본생성자 누락으로 인한 에러 문제  #1

--- src/main/java/com/wjc/codetest/product/model/request/UpdateProductRequest.java
+++ src/main/java/com/wjc/codetest/product/model/request/UpdateProductRequest.java
@@ -3,6 +3,19 @@ package com.wjc.codetest.product.model.request;
 import lombok.Getter;
 import lombok.Setter;
 
+/*
+    [문제] 요청 데이터를 받을 수 없습니다.
+
+    [원인] 기본 생성자가 없으면 스프링이 UpdateProductRequest 객체를 생성할 수 없습니다.
+
+    [개선안]
+        대안:
+            기본 생성자를 작성합니다.
+
+        선택 근거:
+            스프링은 요청 데이터를 객체로 변환할 때 먼저 기본 생성자를 통해 객체를 생성한 뒤 필드값을 세팅합니다.
+            기본 생성자가 없으면 객체 생성이 불가능하기 때문에 오류를 반환합니다.
+ */
 @Getter
 @Setter
 public class UpdateProductRequest {
