d4a84b4687694b12c9e664e7a6231e0f6abeb7ca
조영무 <fprh13@gmail.com>
2025-11-02 22:17:43 +0900

    review: 불필요한 order by 문제  #1

--- src/main/java/com/wjc/codetest/product/service/ProductService.java
+++ src/main/java/com/wjc/codetest/product/service/ProductService.java
@@ -48,6 +48,42 @@ public class ProductService {
         productRepository.delete(product);
     }
 
+    /*
+       [문제] 불필요한 order by 문제
+
+       [원인] 특정 카테고리로 조회하고 카테고리 기준으로 정렬을 진행하고 있습니다.
+
+       [개선안]
+           대안:
+               의미 있는 정렬 기준을 두거나 Sort 조건을 지웁니다.
+           선택 근거:
+               where category = ? 로 이미 동일한 값만 조회된 상태에서
+               order by category를 진행하는것은 논리적인 오류입니다.
+
+               예를 들어, "전자기기" 라는 카테고리를 가진 상품끼리 카테고리 별로 정렬하고 있는것입니다.
+
+       [검증]
+           - Sort.by() 제거 전
+               select
+                   p1_0.product_id, p1_0.category, p1_0.name
+               from
+                   product p1_0
+               where
+                   p1_0.category=?
+               order by
+                   p1_0.category
+               fetch
+                   first ? rows only
+
+           - Sort.by() 제거 후
+               select p1_0.product_id, p1_0.category, p1_0.name
+               from
+                   product p1_0
+               where
+                   p1_0.category=?
+               fetch
+                   first ? rows only
+    */
     public Page<Product> getListByCategory(GetProductListRequest dto) {
         PageRequest pageRequest = PageRequest.of(dto.getPage(), dto.getSize(), Sort.by(Sort.Direction.ASC, "category"));
         return productRepository.findAllByCategory(dto.getCategory(), pageRequest);
