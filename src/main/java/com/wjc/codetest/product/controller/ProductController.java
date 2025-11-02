a2f6b52851deb11af57f004848af5b0d789874d7
조영무 <fprh13@gmail.com>
2025-11-02 20:20:32 +0900

    review: Restful 하지 않습니다  #1

--- src/main/java/com/wjc/codetest/product/controller/ProductController.java
+++ src/main/java/com/wjc/codetest/product/controller/ProductController.java
@@ -13,6 +13,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/*
+    [문제] Restful하지 않은 API 설계로 일관성과 유지보수성이 저하되었습니다.
+
+    [원인] URI의 구성이 표준적이지 않고, Http 메서드가 적절하지 않습니다.
+
+    [개선안]
+        대안:
+            1. API의 행위를 올바른 HTTP 메서드로 표현하고, 자원을 중심으로 URI로 구성합니다.
+                ex)
+                    Get /get/product/by/{productId} -> Get /products/{productId}
+                    Post /create/product -> Post /products
+                    Post /delete/product/{productId} -> Delete /products/{productId}
+                    Post /update/product/{productId} -> Put /products/{productId}
+
+            2. 조회는 GET을 사용하고, 데이터는 바디가 아닌 쿼리 파라미터로 전달합니다.
+                ex)
+                    Post /product/list -> Get /products
+                    @RequestBody GetProductListRequest dto -> @ModelAttribute GetProductListRequest dto
+
+        판단 근거:
+            Rest API의 장점은 누구나 쉽게 이해하고 사용할 수 있다는 점입니다.
+            Http 메서드로 행위를 표현하고, URI로 자원을 표시하기 때문에 요청만 보아도 동작을 예측할 수 있어 협업에 유리합니다.
+
+            또한, "GET 요청시 바디를 이용하지 않고 쿼리 파라미터를 이용한다" 같은 정해진 설계 원칙을 따르기 때문에,
+            모두가 일관된 규칙 아래 개발을 진행 할 수 있어 유지보수성이 높아집니다.
+ */
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
